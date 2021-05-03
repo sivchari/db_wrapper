4c9de09884ad5e62c4ce0191d84fd234b28c15ac
sivchari <shibuuuu5@gmail.com>
2021-05-03 18:19:11 +0900

    implement prepare and exec

--- src/examples/query2.nim
+++ src/examples/query2.nim
@@ -4,4 +4,5 @@ let db = connection.open("mysql", "user:Password!@tcp(127.0.0.1:3306)/database")
 echo db.ping
 echo "Connectted!"
 echo "exec query"
-discard db.query("UPDATE sample SET en_name = ? WHERE id = ?", "nim", 1)
+let stmt = db.prepare("UPDATE sample SET en_name = ? WHERE id = ?")
+discard stmt.exec("NNNim", 1)
